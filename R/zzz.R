d65aa85c8367bb0b869b2c20f1b05a0c5be988ca
skuethe <crazycapivara@gmail.com>
2016-12-04 21:09:33 +0100

    Use onAttach and packageStartupMessage to display startup msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
-.onLoad <- function(libname, pkgname){
-  sprintf("%s %s\n    another crazy way to talk to openweathermap's api\n", pkgname, getNamespaceVersion(pkgname)) %>% cat()
+#.onLoad <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname){
+  #sprintf("%s %s\n    another crazy way to talk to openweathermap's api\n", pkgname, getNamespaceVersion(pkgname)) %>% cat()
+  packageStartupMessage(
+    pkgname, " ", getNamespaceVersion(pkgname), "\n",
+    "   another crazy way to talk to OpenWeatherMap's api\n"
+  )
 }
