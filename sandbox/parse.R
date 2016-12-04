bddaad051386874d508730bbc22bb1dd05c2ce6e
skuethe <crazycapivara@gmail.com>
2016-12-04 17:03:29 +0100

    Test new parser

--- sandbox/parse.R
+++ sandbox/parse.R
@@ -1,6 +1,6 @@
-owmr_parse <- function(response){
+owmr_parse_new <- function(response){
   httr::content(response, as = "text") %>%
-    jsonlite::fromJSON()
+    jsonlite::fromJSON(flatten = T)
 }
 
 get_current_new <- function(city){
@@ -10,10 +10,10 @@ get_current_new <- function(city){
 
 get_forecast_new <- function(city){
   get <- "forecast" %>% owmr_wrap_get()
-  get(city) %>% owmr_parse()
+  get(city) %>% owmr_parse_new()
 }
 
-forecast <- get_forecast("Kassel")
+forecast <- get_forecast_new("Kassel")
 
 forecast$list[,c("dt_txt", "main")] %>%
   as.list() %>% as.data.frame() %>%
