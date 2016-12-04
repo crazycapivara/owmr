be4ff84b03091ab219376818b2c5ee2f3026edd0
skuethe <crazycapivara@gmail.com>
2016-12-04 18:02:23 +0100

    Add func to fetch daily forecast

---
+++ R/forecast-daily.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @param loc city name
+#' @param ... see owm api parameters
+#'
+#' @return list
+#' @export
+#'
+#' @examples \dontrun{
+#'    # 9 days forecast
+#'    get_forecast_daily("London", cnt = 9)
+#' }
+get_forecast_daily <- function(loc = NA, ...){
+  get <- owmr_wrap_get("forecast/daily")
+  get(loc, ...) %>% owmr_parse()
+}
