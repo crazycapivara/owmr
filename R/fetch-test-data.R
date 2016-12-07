0eec4968ef0033c3a0e11530ab46d3fad86bd38a
skuethe <crazycapivara@gmail.com>
2016-12-07 20:13:18 +0100

    Add helper to fetch test data for multiple cities

--- R/fetch-test-data.R
+++ R/fetch-test-data.R
@@ -1,5 +1,17 @@
 folder_test_data <- "tests/testthat/data/"
 
+# save data to test folder
+save_test_data <- function(x, filename){
+  (fn <- paste0(folder_test_data, filename)) %>%
+    saveRDS(x, .)
+  sprintf("data written to %s", fn) %>% cat
+}
+
+save_current_multiple <- function(){
+  get_current_for_group(c(2831088, 2847639, 2873291)) %>%
+    save_test_data("current_weather_multiple.rds")
+}
+
 #' helper function to fetch test data
 #'
 #' helper function to update test data
