c26825ac8de3ece6ec64863bb7b7ff4713563e48
skuethe <crazycapivara@gmail.com>
2016-12-09 18:41:49 +0100

    Fix bug with marked UTF-8 string in data

--- helpers/get_owm_city_list.R
+++ helpers/get_owm_city_list.R
@@ -2,5 +2,10 @@ owm_city_list <- "http://openweathermap.org/help/city_list.txt"
 filename <- "data-raw/city_list.txt"
 
 download.file(owm_city_list, filename)
-owm_cities <- data.table::fread(filename, encoding = "UTF-8", data.table = F)
+#owm_cities <- data.table::fread(filename, encoding = "UTF-8", data.table = F)
+owm_cities <- data.table::fread(filename, data.table = FALSE)
 owm_cities[is.na(owm_cities)] <- "None"
+owm_cities[7053, ] %>% print
+# run lines above in case of wrong encoding
+#nm_7053 <- "Metabetchouan-Lac-a-la-Croix"
+#owm_cities[7053, ]$nm <- nm_7053
