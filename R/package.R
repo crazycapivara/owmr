553776515f1eb67fee80ea5cb9737b41c567a043
skuethe <crazycapivara@gmail.com>
2016-12-09 22:01:55 +0100

    Update package documentation

--- R/package.R
+++ R/package.R
@@ -1,3 +1,13 @@
+#' owmr - An R interface to OpenWeatherMap
+#'
+#' @name owmr
+#'
+#' @examples \dontrun{
+#'    get_current("Kassel", units = "metric")
+#'    get_forecast("London", cnt = 7)
+#'    # ...
+#' }
+#'
 #' @importFrom magrittr %>% %<>%
 #' @export %>%
 NULL
