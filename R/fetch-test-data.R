1e48dd63b4da211dde4e8d908b748f94ad9d04b6
skuethe <crazycapivara@gmail.com>
2016-12-09 20:18:15 +0100

    Remove dead code

--- R/fetch-test-data.R
+++ R/fetch-test-data.R
@@ -36,18 +36,11 @@ fetch_test_data <- function(run_tests = TRUE){
   if(is.null(.pkg_env$api_key)){
     stop("Set api key before trying to fetch data!", call. = F)
   }
-  # fetch current data
   save_current()
-  #current_weather <- get_current("Kassel")
-  #save(current_weather,
-  #     file = paste0(folder_test_data, "current_weather.rda"))
-  # fetch data from 15 stations around geo point
-  #stations_data <- find_stations_by_geo_point(lat = 51.31667, lon = 9.5, cnt = 15)
-  #save(stations_data,
-  #     file = paste0(folder_test_data, "stations_data.rda"))
-  #sprintf("data files saved to %s\n", folder_test_data) %>% cat
-  # fetch data for 3 cities at once
   save_current_multiple()
   save_stations_multiple()
-  if(run_tests) testthat::test_dir("tests/testthat", reporter = "tap")
+  # run tests
+  if(run_tests){
+    testthat::test_dir("tests/testthat", reporter = "tap")
+  }
 }
