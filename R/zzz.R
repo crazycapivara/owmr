631419b0edc9fe94a812f4aea41b1eb7c1cd192d
skuethe <crazycapivara@gmail.com>
2016-12-09 21:23:46 +0100

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
-#.onLoad <- function(libname, pkgname){
 .onAttach <- function(libname, pkgname){
-  #sprintf("%s %s\n    another crazy way to talk to openweathermap's api\n", pkgname, getNamespaceVersion(pkgname)) %>% cat()
   packageStartupMessage(
     pkgname, " ", getNamespaceVersion(pkgname), "\n",
-    "   another crazy way to talk to OpenWeatherMap's api\n"
+    "   another crazy way to talk to OpenWeatherMap's api\n",
+    "   Documentation: type ?owmr or https://crazycapivara.github.io/owmr/\n",
+    "   Issues, notes and bleeding edge: https://github.com/crazycapivara/owmr/\n"
   )
 }
