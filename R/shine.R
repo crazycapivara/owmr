0619e7999ea7add974820b2d922cdfe39c530dce
skuethe <crazycapivara@gmail.com>
2016-12-10 14:54:28 +0100

    Add popups to markers in shiny app

--- R/shine.R
+++ R/shine.R
@@ -1,9 +1,30 @@
+keys_default <- c(
+  "name",
+  "coord.lon",
+  "coord.lat",
+  "main.temp"
+)
+
+helper_popups <- function(x, keys){
+  x %<>% unlist() %>% .[keys] %>% unname()
+  sapply(1:length(x), function(i){
+    sprintf("<b>%s</b>: %s", keys[i], x[i])
+  }) %>% paste0(collapse = "</br>")
+}
+
 #' shiny app to display weather data from owm
+#'
+#' @param title header of app
+#' @param keys values, which should appear in popup
+#'
+#' @return shiny app
 #'
 #' @export
 #'
-owmr_shine <- function(title = "owmr -shine"){
+owmr_shine <- function(title = "owmr -shine", keys = keys_default){
   loc <- search_city_list("Kassel")[1, ]
+  # x == DATA
+  DATA <- get_current("Kassel", units = "metric")
 
   view <- shiny::fluidPage(
     shiny::h1(title),
@@ -15,7 +36,7 @@ owmr_shine <- function(title = "owmr -shine"){
     output$map <- leaflet::renderLeaflet({
       leaflet::leaflet() %>% leaflet::addTiles() %>%
         leaflet::setView(loc$lon, loc$lat, zoom = 12) %>%
-        leaflet::addMarkers(loc$lon, loc$lat)
+        leaflet::addMarkers(loc$lon, loc$lat, popup = helper_popups(DATA, keys))
     })
   }
   shiny::shinyApp(view, controller, options = list(port = 12345))
