33e14b2173dcb91abbce47f2e14cf78c0b42bc23
skuethe <crazycapivara@gmail.com>
2016-12-15 21:28:10 +0100

    Add func to wrap html tag around a value

--- R/utils-popup.R
+++ R/utils-popup.R
@@ -1,39 +1,21 @@
-build_popups <- function(x, ...){
+popup <- function(x, ...){
   cmd <- substitute(paste0(...))
   with(x, eval(cmd))
 }
 
-`%wrap%` <- function(tag, value){
+#' Wrap html tag around value.
+#'
+#' @param tag tag like \code{i}, \code{strong} or whatever
+#' @param value value to be wrapped around
+#'
+#' @return wrapped value
+#' @export
+#'
+#' @examples
+#' x <- get_current("London", units = "metric")
+#' # create a simple popup to show up on a (leaflet) map
+#' popup <- wrap("strong", x$main$temp)
+#'
+wrap <- function(tag, value){
   sprintf("<%s>%s</%s>", tag, value, tag)
-}
-
-strong <- function(value){
-  "strong" %wrap% value
-}
-
-tag <- list(
-  i = function(x) "i" %wrap% x,
-  b = function(x) "b" %wrap% x
-)
-
-popups <- function(data = NULL){
-  l = list()
-  attr(l, "data") <- data
-  l
-}
-
-tag_ <- function(popups, name, x){
-  if(class(x) == "formula"){
-    key <- terms(x) %>% labels()
-    x <- attr(popups, "data") %>% .[[key]]
-  }
-  c(popups, list(name %wrap% x))
-}
-
-txt <- function(popups, x){
-  c(popups, list(x))
-}
-
-create_ <- function(popups){
-  do.call(paste0, popups)
 }
