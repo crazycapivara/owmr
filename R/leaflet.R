19f90ce517842d677c3c465e65f31e54710be07d
skuethe <crazycapivara@gmail.com>
2016-12-17 15:19:51 +0100

    Add func to show weather data on leaflet map

---
+++ R/leaflet.R
@@ -0,0 +1,26 @@
+#render <- function(template, data){
+#  # add empty column for correct subsetting of data frames with only one column
+#  if(ncol(data) == 1) {
+#    random_name <- tempfile(pattern = "", tmpdir = "") %>%
+#      sub("/", "", .)
+#    data[random_name] <- 0L
+#  }
+#  sapply(1:nrow(data), function(i){
+#    whisker::whisker.render(template, data[i, ])
+#  })
+#}
+#
+#`%$%` <- render
+
+add_weather <- function(map, data = NULL, lng = NULL, lat = NULL, ...){
+  if(is.null(lng) | is.null(lat)){
+    if(!is.data.frame(data)){
+      lng <- data$coord$lon
+      lat <- data$coord$lat
+    } else{
+      lng = ~coord.lon
+      lat = ~coord.lat
+    }
+  }
+  leaflet::addMarkers(map, lng, lat, data = data, ...)
+}
