6f2c5f17de1dd29849b424160b751984425f7f44
skuethe <crazycapivara@gmail.com>
2016-12-17 15:36:38 +0100

    Update and document render func

--- R/render.R
+++ R/render.R
@@ -1,3 +1,20 @@
+#' Render template.
+#'
+#' Vectorizes \code{\link[whiskers]{whisker.render}}.
+#'
+#' @param template template
+#' @param data data frame where column names correspond to
+#'    variables names in template
+#'
+#' @return rendered template
+#' @aliases "%$%"
+#' @export
+#'
+#' @seealso \code{\link[whiskers]{whisker.render}}
+#'
+#' @examples
+#' vars <- data.frame(a = 1:3, b = 23:21)
+#' "a = {{a}} and b = {{b}}" %$% vars
 render <- function(template, data){
   # add empty column for correct subsetting of data frames with only one column
   if(ncol(data) == 1) {
