ca67898cfc8702dfbf450f5e48b1ccb1a1169505
skuethe <crazycapivara@gmail.com>
2016-12-18 20:03:10 +0100

    Update funcs to flatten weather column

--- R/flatten_.R
+++ R/flatten_.R
@@ -1,18 +1,37 @@
 #' Flatten nested data frames.
 #'
-#' Just wraps \code{\link[tidyr]{unnest}} (package \pkg{tidyr}
-#' setting paremeter \code{sep = "."} by default.
+#' Wraps \code{\link[tidyr]{unnest}} from package \pkg{tidyr}
+#' setting parameter \code{sep = "."} by default.
 #'
-#' @param data data frame
-#' @param sep separator used to combine nested (list) column names with column
-#'    names of nested data frames
+#' @param data data frame containing nested data frames
+#' @param sep separator used to combine flattened column names
+#' with column names of nested data frames
 #'
-#' @return unnested/flattened data frame
+#' @return flattened data frame
 #' @export
 #'
 #' @seealso \code{\link[tidyr]{unnest}}
 #'
-## @examples
-flatten_ <- function(data, sep = "."){
-  tidyr::unnest(data, .sep = "_")
+#' @examples \dontrun{
+#'    find_city("Malaga")$list %>% knock_down()
+#' }
+knock_down <- function(data, sep = "."){
+  tryCatch(
+    loadNamespace("tidyr"),
+    error = function(e){
+      stop("Install 'tidyr' if you want to knock down data frames.", call. = F)
+    })
+  result <- tidyr::unnest(data, .sep = sep)
+  # in case there is more than one row in unnested data frames,
+  # keep first row only
+  index <- seq(1, nrow(result), nrow(result)/nrow(data))
+  result[index, ]
+}
+
+knock_down_weather <- function(data, sep = "."){
+  weather_column <- "weather"
+  weather <- flatten_weather(data[, weather_column])
+  names(weather) %<>% paste0(weather_column, sep, . )
+  data$weather <- NULL
+  cbind(data, weather)
 }
