cee2976bc528305e9e8c1f51c976e1c88c49e9ec
skuethe <crazycapivara@gmail.com>
2016-12-19 21:29:55 +0100

    Document and export tidy_up func

--- R/tidy_up.R
+++ R/tidy_up.R
@@ -1,4 +1,21 @@
-tidy_up <- function(data, use_underscore = TRUE, remove_prefix = c("main")){
+#' Tidy up owm data.
+#'
+#' @param data response (list) returned from owm
+#' @param use_underscore substitute dots in column names with an underscore
+#' @param remove_prefix prefices to be removed for shorter column names
+#'    (\code{remove_prefix = NULL}) will keep all prefices
+#'
+#' @return response with 'clean' data frame
+#' @export
+#'
+#' @examples \dontrun{
+#'    result <- find_city("Malaga") %>% tidy_up()
+#'    # keep dots in column names
+#'    result <- find_city("Malaga") %>% tidy_up(use_underscore = FALSE)
+#'    # keep all prefices
+#'    result <- find_city("Malaga") %>% tidy_up(remove_prefix = NULL)
+#' }
+tidy_up <- function(data, use_underscore = TRUE, remove_prefix = c("main", "sys")){
   # flatten weather
   data$list %<>% cbind(weather = flatten_weather(.[, "weather"]))
   data$list$weather = NULL
@@ -13,6 +30,5 @@ tidy_up <- function(data, use_underscore = TRUE, remove_prefix = c("main")){
   if(use_underscore){
     names(data$list) %<>% gsub("\\.", "_", .)
   }
-  #names(data$list) %<>% gsub("main.", "", .) %>% gsub("\\.", "_", .)
   data
 }
