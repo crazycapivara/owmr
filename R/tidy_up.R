08ca3a5cc08cfc8eee2fd6ab1096df15cbdbbd4b
skuethe <crazycapivara@gmail.com>
2016-12-21 08:18:56 +0100

    Split func tidy_up

--- R/tidy_up.R
+++ R/tidy_up.R
@@ -1,3 +1,39 @@
+#' Remove prefices from column names.
+#'
+#' @param data data frame
+#' @param prefices vector of prefices to be removed from column names
+#' @param sep prefix separator
+#'
+#' @return data frame with updated column names
+#' @export
+#'
+#' @examples
+#' x <- data.frame(main.temp = 1:10, sys.msg = "OK", cnt = 10:1)
+#' names(x)
+#' remove_prefix(x, c("main", "sys")) %>% names
+remove_prefix <- function(data, prefices, sep = "."){
+  for(prefix in prefices){
+    prefix <- paste0(prefix, sep)
+    names(data) %<>% gsub(prefix, "", .)
+  }
+  data
+}
+
+#' Substitute dots with underscores in column names.
+#'
+#' @param data data frame
+#'
+#' @return data frame with updated column names
+#' @export
+#'
+#' @examples
+#' names(airquality)
+#' use_underscore(airquality) %>% names
+use_underscore <-function(data){
+  names(data) %<>% gsub("\\.", "_", .)
+  data
+}
+
 #' Tidy up owm data.
 #'
 #' @param data response (list) returned from owm
