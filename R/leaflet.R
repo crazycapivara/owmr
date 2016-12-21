193a4820aae988e382fd6f6dfa2f2162e9079166
skuethe <crazycapivara@gmail.com>
2016-12-21 15:36:17 +0100

    Update add_weather

--- R/leaflet.R
+++ R/leaflet.R
@@ -26,16 +26,9 @@
 #' }
 add_weather <- function(map, data, lng = NULL, lat = NULL, icon = NULL, template = NULL, popup = NULL, ...){
   if(is.null(lng) | is.null(lat)){
-    if(!is.data.frame(data)){
-      lng <- data$coord$lon
-      lat <- data$coord$lat
-    } else{
-      names_ = names(data)
-      lng <- names_[grep("lon", names_)] %>% data[[.]]
-      lat <- names_[grep("lat", names_)] %>% data[[.]]
-      #lng = ~coord.lon
-      #lat = ~coord.lat
-    }
+    names_ = names(data)
+    lng <- names_[grep("lon", names_)] %>% data[[.]]
+    lat <- names_[grep("lat", names_)] %>% data[[.]]
   }
   if(!is.null(icon)){
     icon %<>% get_icon_url() %>%
