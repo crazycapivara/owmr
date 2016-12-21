cea12f41490f85f067d399e7e0058a80a3530f57
skuethe <crazycapivara@gmail.com>
2016-12-21 14:08:52 +0100

    Add func flatten for responses from get_current

---
+++ R/tidy_up-flatten.R
@@ -0,0 +1,16 @@
+#' Flatten list.
+#'
+#' @param data list returned from owm
+#'
+#' @return flattened list
+#' @export
+#'
+#' @examples \dontrun{
+#'    get_current("Rio de Janeiro") %>% flatten()
+#'    get_current("Rio de Janeiro") %>% flatten() %>%
+#'       tidy_up_()
+#' }
+flatten <- function(data){
+  as.data.frame(data, stringsAsFactors = FALSE)[1, ] %>%
+    as.list()
+}
