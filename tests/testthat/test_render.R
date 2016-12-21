16c6e54e97b0240cf95813a95f6bfe5466507bc3
skuethe <crazycapivara@gmail.com>
2016-12-21 15:54:33 +0100

    Add tests for render operator

---
+++ tests/testthat/test_render.R
@@ -0,0 +1,24 @@
+context("render operator")
+
+test_that("data frame input", {
+  # when
+  df <- iris[1:5, ]
+  result <- "{{Species}}: {{Sepal_Length}}" %$$%
+    df
+  # then
+  expected_cnt <- 5
+  expected_values <- c("setosa: 5.1", "setosa: 4.9")
+  # -----
+  expect_equal(length(result), expected_cnt)
+  expect_equal(result[1:2], expected_values)
+})
+
+test_that("list input", {
+  # when
+  l <- list(name = "stef" ,country = "DE")
+  result = "{{name}} lives in {{country}}" %$$% l
+  # then
+  expected <- "stef lives in DE"
+  # -----
+  expect_equal(result, expected)
+})
