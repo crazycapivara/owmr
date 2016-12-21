b27f05d0861cc69f04fc735f3162b2bc97d3e94f
skuethe <crazycapivara@gmail.com>
2016-12-21 12:23:26 +0100

    Add tests for tidy up funcs

---
+++ tests/testthat/test_tidy_up.R
@@ -0,0 +1,23 @@
+context("tidy up data")
+
+test_that("remove prefix", {
+  # when
+  current_multiple <- readRDS("data/current_multiple.rds")$list %>%
+    remove_prefix(c("main", "sys"))
+  names_ <- names(current_multiple)
+  # then
+  contains_temp_min <- "temp_min" %in% names_
+  contains_sunrise <- "sunrise" %in% names_
+  expect_true(contains_temp_min)
+  expect_true(contains_sunrise)
+})
+
+test_that("tidy up all", {
+  # when
+  current_multiple <- readRDS("data/current_multiple.rds") %>%
+    tidy_up()
+  names_ <- names(current_multiple$list)
+  # then
+  contains_weather_icon <- "weather_icon" %in% names_
+  expect_true(contains_weather_icon)
+})
