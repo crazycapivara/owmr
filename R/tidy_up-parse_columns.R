9d49cdc84a3f970ac3e57b512926dfee151803f9
skuethe <crazycapivara@gmail.com>
2016-12-23 09:40:19 +0100

    Rename parse_result to parse_columns

--- R/tidy_up-parse_columns.R
+++ R/tidy_up-parse_columns.R
@@ -1,6 +1,6 @@
 #' Apply functions to columns.
 #'
-#' @param x data frame
+#' @param data data frame
 #' @param functions_ named list where keys correspond to column names
 #'
 #' @return updated data frame
@@ -11,9 +11,9 @@
 #'    forecast <- get_forecast("Kassel")$list
 #'    forecast %<>% parse_result(list(dt = parse_dt))
 #' }
-parse_result <- function(x, functions_){
+parse_columns <- function(data, functions_){
   for(name in names(functions_)){
-    x[[name]] %<>% functions_[[name]]()
+    data[[name]] %<>% functions_[[name]]()
   }
   x
 }
