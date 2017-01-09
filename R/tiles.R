7b1af2ab523c582895838eacf272656a4459387e
skuethe <crazycapivara@gmail.com>
2017-01-09 19:57:24 +0100

    Document and export add_owm_tiles func

--- R/tiles.R
+++ R/tiles.R
@@ -1,8 +1,8 @@
 owm_tile_server <- 'http://{s}.tile.openweathermap.org/map/%s/{z}/{x}/{y}.png'
 
-#' owm layers
+#' Available owm layers.
 #'
-#' @return list with available owm layers
+#' @return list of available owm layers
 #' @export
 #'
 owm_layers <- function(){
@@ -25,6 +25,22 @@ get_owm_tile_template <- function(layer_name = "temp"){
   sprintf(owm_tile_server, layer_name)
 }
 
+#' Add owm tiles to leaflet map.
+#'
+#' @param map leaflet map object
+#' @param layer_name owm layer name,
+#'    see \code{\link{owm_layers}}
+#' @param ... optional parameters passed to
+#'    \code{\link[leaflet]{addTiles}}
+#'
+#' @return updated map object
+#' @export
+#'
+#' @examples \dontrun{
+#'    leaflet() %>% add_owm_tiles() %>%
+#'       addMarkers(data = quakes[1:20, ])
+#' }
+#'
 add_owm_tiles <- function(map, layer_name = "temp", ...){
   tile_template <- get_owm_tile_template(layer_name)
   leaflet::addTiles(map, tile_template, ...)
