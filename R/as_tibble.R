340d3a052faa9c34d58e742094edeab8667f5935
aedobbyn <adobbs11@gmail.com>
2018-09-01 18:28:21 -0500

    add skeletons

--- R/as_tibble.R
+++ R/as_tibble.R
@@ -1,5 +1,29 @@
 #' Parse owm data to tibble object
 #'
+#' @param data result returned from \code{\link{get_current}}
+#'   or \code{\link{get_forecast}}
+#'
+#' @name as_tibble
+NULL
+
+#' @rdname as_tibble
+#' @export
+current_as_tibble <- function(data) {
+  message("not implemented yet")
+  data
+}
+
+
+#' @rdname as_tibble
+#' @export
+forecast_as_tibble <- function(data) {
+  message("not implemented yet")
+  data
+}
+
+
+#' Parse owm data to tibble object
+#'
 #' @param resp result returned from \code{\link{get_current}}
 #'   or \code{\link{get_forecast}}
 #'
