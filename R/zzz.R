ebcf45f110f20542b065a0176edb7a0bfcd47da6
skuethe <crazycapivara@gmail.com>
2018-10-07 18:33:07 +0200

    Show api key message only when not set

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,16 @@
 .onAttach <- function(libname, pkgname) {
+  api_key_message <- c(
+    "\n",
+    "It is recommended that you store your api key in an environment variable called OWM_API_KEY.\n"
+  )
+  if (!identical(Sys.getenv("OWM_API_KEY"), "")) api_key_message <- ""
+
   packageStartupMessage(
     pkgname, " ", getNamespaceVersion(pkgname), "\n",
     "   another crazy way to talk to OpenWeatherMap's API\n",
     "   Documentation: type ?owmr or https://crazycapivara.github.io/owmr/\n",
-    "   Issues, notes and bleeding edge: https://github.com/crazycapivara/owmr/\n\n",
-    "   it is recommended that you store your api key in an environment variable\n",
-    "   called OWM_API_KEY"
+    "   Issues, notes and bleeding edge: https://github.com/crazycapivara/owmr/\n",
+    api_key_message
   )
 }
 
