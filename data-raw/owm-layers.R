341cf0dbc0f436c66044628b26df68df9649087a
skuethe <crazycapivara@gmail.com>
2018-11-01 23:12:31 +0100

    Add 'owm_layers' dataset

---
+++ data-raw/owm-layers.R
@@ -0,0 +1,18 @@
+owm_layers <- list(
+  Precipitation_new = "precipitation_new",
+  Precipitation = "precipitation",
+  Precipitation_classic_style = "precipitation_cls",
+  Rain = "rain",
+  Rain_classic_style = "rain_cls",
+  Snow = "snow",
+  Clouds_new = "clouds_new",
+  Clouds = "clouds",
+  Cloud_classic_style = "clouds_cls",
+  Sea_level_pressure_new = "pressure_new",
+  Sea_level_pressure = "pressure",
+  Sea_level_pressure_contour = "pressure_cntr",
+  Temperature_new = "temp_new",
+  Temperature = "temp",
+  Wind_speed_new = "wind_new",
+  Wind_speed = "wind"
+)
