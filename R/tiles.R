1d828954e3a0476fe4f649568991aaeba93968b5
skuethe <crazycapivara@gmail.com>
2018-11-01 18:50:35 +0100

    Add new layers to 'owm_layers' list

--- R/tiles.R
+++ R/tiles.R
@@ -7,21 +7,26 @@ owm_tile_server <- "http://{s}.tile.openweathermap.org/map/%s/{z}/{x}/{y}.png?ap
 #'
 owm_layers <- function() {
   list(
+    Precipitation_new = "precipitation_new",
     Precipitation = "precipitation",
     Precipitation_classic_style = "precipitation_cls",
     Rain = "rain",
     Rain_classic_style = "rain_cls",
     Snow = "snow",
+    Clouds_new = "clouds_new",
     Clouds = "clouds",
     Cloud_classic_style = "clouds_cls",
+    Sea_level_pressure_new = "pressure_new",
     Sea_level_pressure = "pressure",
     Sea_level_pressure_contour = "pressure_cntr",
+    Temperature_new = "temp_new",
     Temperature = "temp",
+    Wind_speed_new = "wind_new",
     Wind_speed = "wind"
   )
 }
 
-get_owm_tile_template <- function(layer_name = "temp") {
+get_owm_tile_template <- function(layer_name) {
   sprintf(owm_tile_server, layer_name, get_api_key())
 }
 
@@ -30,8 +35,7 @@ get_owm_tile_template <- function(layer_name = "temp") {
 #' @param map leaflet map object
 #' @param layer_name owm layer name,
 #'    see \code{\link{owm_layers}}
-#' @param ... optional parameters passed to
-#'    \code{\link[leaflet]{addTiles}}
+#' @param ... optional parameters passed to \code{\link[leaflet]{addTiles}}
 #'
 #' @return updated map object
 #' @export
@@ -41,7 +45,7 @@ get_owm_tile_template <- function(layer_name = "temp") {
 #'       addMarkers(data = quakes[1:20, ])
 #' }
 #'
-add_owm_tiles <- function(map, layer_name = "temp", ...) {
+add_owm_tiles <- function(map, layer_name = owm_layers()$Temperature_new, ...) {
   tile_template <- get_owm_tile_template(layer_name)
   leaflet::addTiles(map, tile_template, ...)
 }
